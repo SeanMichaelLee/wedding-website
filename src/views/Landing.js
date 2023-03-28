7d764d20e47bace058a1d7fd9518c00b8d115d6f
Sean Lee <seanathanmlee@gmail.com>
2023-03-27 22:15:22 -0400

    Adding our story

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -69,10 +69,7 @@ class Landing extends React.Component {
                                         <Typography variant="h5" className={classes.subtitle}>
                                             Summer 2015 - College Sweethearts
                                         </Typography>
-                                        <Typography variant="body1" color="text.secondary">As freshman at the University of Central Florida, we had mutural friends that conviced us coming out of soccer retirement was a good idea.
-                                            Little did we know, the intramural soccer fields would be the kick-off of our life-long relationship. I like to think that Camille knew I was a keeper
-                                            as soon as she saw how goal-oriented, but only after a few dates at UCF's dinning hall I was able to sweep Camille off her feet.
-                                            For the next four years, Camille showed support and patiences with my gruelying Compute Science curriculum and I continued to motivate Camille that going to class was important.
+                                        <Typography variant="body1" color="text.secondary">As freshman at the University of Central Florida, Camille and Sean had mutual friends that convinced them coming out of soccer retirement was a good idea. Little did they know the intramural soccer fields would be the kick-off of their life-long relationship. A few dates at the UCF dinning hall, was all it took for them to dive head first into dating. It’s true opposites attract, while Camille was studying wine tasting and theme park design, Sean was spending long days studying computational algorithms and machine learning. While Camille graduated and landed her first big job in South Florida. Sean continued mastering computer science at UCF. Sean and Camille were forced to have a long distance relationship, which meant for the first time ever Sean had to do his own laundry. 
                                         </Typography>
                                     </Grid>
                                     </TimelineContent>
@@ -90,10 +87,10 @@ class Landing extends React.Component {
                                     <TimelineConnector />
                                     </TimelineSeparator>
                                     <TimelineContent sx={{ py: '12px', px: 2 }}>
-                                    <Typography variant="h5" component="span">
+                                    <Typography variant="h5" className={classes.subtitle}>
                                         Fall 2020 - Reunited in the State of Lovers
                                     </Typography>
-                                    <Typography>Blah Blah Blah</Typography>
+                                    <Typography variant="body1" color="text.secondary">After distance made the heart grow founder, Sean decided to accept a job even further away in Washington DC. Camille didn’t want to miss out on an opportunity to experience seasons, so she packed her bag and found a new job in Washington DC six months later. After reuniting in the State of Lovers, Sean and Camille decided to become business partners and buy their first home. Sean and Camille are now convinced that if home ownership won’t test your relationship, building Ikea furniture will.</Typography>
                                     </TimelineContent>
                                 </TimelineItem>
                                 <TimelineItem>
@@ -109,10 +106,10 @@ class Landing extends React.Component {
                                     <TimelineConnector />
                                     </TimelineSeparator>
                                     <TimelineContent sx={{ py: '12px', px: 2 }}>
-                                    <Typography variant="h5" component="span">
+                                    <Typography variant="h5" className={classes.subtitle}>
                                         Summer 2022 - The Proposal
                                     </Typography>
-                                    <Typography>Blah Blah Blah</Typography>
+                                    <Typography variant="body1" color="text.secondary">With the blessing of Camille’s parents, but a single contingency of Sean getting a Costco membership  (which he still doesn’t have). Sean started to plan the proposal. Camille always dropped “subtle” hints about getting proposed to in France. Luckily, Sean and Camille were taking a trip to France. Sean surprised Camille, with a drive to watch the sunrise over the Mediterranean. Sean got down on one knew and blacked out, but it started with “Camille ma Cherie” and ended with oui. </Typography>
                                     </TimelineContent>
                                 </TimelineItem>
                                 <TimelineItem>
@@ -128,10 +125,10 @@ class Landing extends React.Component {
                                     <TimelineConnector />
                                     </TimelineSeparator>
                                     <TimelineContent sx={{ py: '12px', px: 2 }}>
-                                    <Typography variant="h5" component="span">
+                                    <Typography variant="h5" className={classes.subtitle}>
                                         April 24, 2024 - Celebrate Our Marriage!
                                     </Typography>
-                                    <Typography>Blah Blah Blah</Typography>
+                                    <Typography variant="body1" color="text.secondary">One of Camille and Sean’s favorite hobbies is to travel, and their favorite city is Paris, so what better way to celebrate their marriage than to bring everyone to their favorite destination! It just so happens, that both Sean and Camille have family that have lived in or around Paris, which makes it a meaningful destination for not only Sean and Camille, but also their families. We are excited to celebrate with everyone and explore Paris! </Typography>
                                     </TimelineContent>
                                 </TimelineItem>
                             </Timeline>
